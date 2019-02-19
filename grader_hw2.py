399dc0acf268379de1f137e2238dbcc6ce91e568
Eleanor Quint <pquint@cse.unl.edu>
2019-02-19 10:52:26 -0600

    Update to make things work?

--- grader_hw2.py
+++ grader_hw2.py
@@ -1,6 +1,12 @@
 import argparse
+import json
+import os
 
-parser = argparse.ArgumentParser(description='Grade homework 1.')
+import numpy as np
+
+import util
+
+parser = argparse.ArgumentParser(description='Grade homework 2.')
 parser.add_argument(
     '--team_file',
     default='teams.json',
@@ -9,7 +15,43 @@ parser.add_argument(
 parser.add_argument(
     '--data_dir',
     type=str,
-    default='./data/fashion/',
+    default='./data/cifar100/',
     help='directory where data is located')
-parser.add_argument('--handin_dir', type=str, default='./submissions/HW2/')
+parser.add_argument(
+    '--handin_dir', type=str, default='./submissions/hw2_public_handin/')
+parser.add_argument('--path_prefix', type=str, default='homework_2')
 args = parser.parse_args()
+
+if __name__ == "__main__":
+    cifar100_train_data = np.load('cifar_images.npy')
+    cifar100_train_labels = np.load('cifar_labels.npy')
+    cifar100_test_data = np.load('cifar_images_test.npy')
+    cifar100_test_labels = np.load('cifar_labels_test.npy')
+    train_pair = (cifar100_train_data, cifar100_train_labels)
+    test_pair = (cifar100_test_data, cifar100_test_labels)
+
+    with open(args.team_file, 'r') as team_file:
+        team_list = json.load(team_file)['teams']
+
+    team_dict = util.score_classification_teams(
+        train_pair, test_pair, team_list, args.handin_dir, args.path_prefix)
+
+    # combine existing data with new
+    if os.path.isfile('homework2_scores.json'):
+        with open('homework2_scores.json', 'r') as json_file:
+            leaderboard_dict = json.load(json_file)
+
+        for key in team_dict.keys():
+            leaderboard_dict[key] = {
+                **leaderboard_dict.get(key, dict()),
+                **team_dict[key]
+            }
+    else:
+        leaderboard_dict = team_dict
+
+    # write data
+    with open('homework2_scores.json', 'w') as json_file:
+        new_json = json.dumps(leaderboard_dict, sort_keys=True)
+        json_file.write(new_json)
+    print(list(team_dict.keys()))
+    print(team_dict)
--- util.py
+++ util.py
@@ -1,7 +1,79 @@
 import datetime
 import os
+import json
 
 import tensorflow as tf
+
+
+def score_classification_teams(train_pair, test_pair, team_list, handin_dir,
+                               path_prefix):
+    # load datasets
+    X_train, y_train = train_pair
+    X_test, y_test = test_pair
+
+    # score assignments
+    team_dict = dict()
+    unix_now = unix_time_millis(datetime.datetime.now())
+    for team in team_list:
+        team_name = list(team.keys())[0]
+        for username in list(team.values())[0]:
+            try:
+                # train
+                train_accuracy, train_confusion_matrix = score_classification(
+                    os.path.join(handin_dir, username), X_train, y_train,
+                    path_prefix)
+                train_dict = {
+                    'accuracy': float(train_accuracy),
+                    'confusion_matrix': train_confusion_matrix.tolist()
+                }
+                # test
+                test_accuracy, test_confusion_matrix = score_classification(
+                    os.path.join(handin_dir, username), X_test, y_test,
+                    path_prefix)
+                test_dict = {
+                    'accuracy': float(test_accuracy),
+                    'confusion_matrix': test_confusion_matrix.tolist()
+                }
+                # metadata
+                metadata_dict = {}
+                # combined score dict
+                score_dict = {
+                    'train': train_dict,
+                    'test': test_dict,
+                    'metadata': metadata_dict
+                }
+                team_dict[team_name] = {str(unix_now): score_dict}
+                break
+            except IOError as e:
+                print(e)
+                train_dict = {
+                    'accuracy': float(0.),
+                    'confusion_matrix': [[0.]]
+                }
+                test_dict = {'accuracy': float(0.), 'confusion_matrix': [[0.]]}
+                metadata_dict = {'error': str(e)}
+                score_dict = {
+                    'train': train_dict,
+                    'test': test_dict,
+                    'metadata': metadata_dict
+                }
+                team_dict[team_name] = {str(unix_now): score_dict}
+            except KeyError as e:
+                print(e)
+                train_dict = {
+                    'accuracy': float(0.),
+                    'confusion_matrix': [[0.]]
+                }
+                test_dict = {'accuracy': float(0.), 'confusion_matrix': [[0.]]}
+                metadata_dict = {'error': str(e)}
+                score_dict = {
+                    'train': train_dict,
+                    'test': test_dict,
+                    'metadata': metadata_dict
+                }
+                team_dict[team_name] = {str(unix_now): score_dict}
+                break
+    return team_dict
 
 
 def score_classification(model_directory, data, labels, path_prefix):
